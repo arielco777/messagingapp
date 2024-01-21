2fb2797e94ffba2726c72987d772ac4fb504d50b
ariel <arielco777@gmail.com>
2024-01-21 15:11:20 -0500

    input design

--- components/ui/Messaging.tsx
+++ components/ui/Messaging.tsx
@@ -77,7 +77,7 @@ const Messaging = () => {
                         className="w-5/6 md:w-1/3 h-10 border border-neutral-400 rounded flex justify-between"
                     >
                         <input
-                            className="focus:outline-none rounded pl-2"
+                            className="focus:outline-none rounded px-2 w-full bg-red-100"
                             type="text"
                             name="message"
                         />
